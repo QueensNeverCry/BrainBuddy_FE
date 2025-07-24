5ba51fc12b430b8cefa469ee08752ef63e96c457
syyyyp <christieforsy@gmail.com>
2025-07-24 17:50:42 +0900

    회원가입 작성 완료

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import MainLogo from "./components/landing/MainLogo";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <MainLogo />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
